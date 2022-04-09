be9442389257ea9dd543a5d0ee0759f65cdc263b
Terry Simcox <terryjsimcox@gmail.com>
2022-04-09 00:14:01 -0500

    Fixed errors in AppContext.

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,53 +10,53 @@ import { colors, fonts } from '../containts/styles.defaults';
 
 export default function Home() {
   const { state } = useApp();
-  const movieList = [
-    {
-      category: 'Now Showing',
-      title: 'Father Stu',
-      poster: '/images/FatherStu.jpg',
-    },
-    {
-      category: 'Now Showing',
-      title: 'Fantastic Beasts',
-      poster: '/images/FantasticBeasts.jpg',
-    },
-    {
-      category: 'Now Showing',
-      title: 'Ambulance',
-      poster: '/images/Ambulance.jpg',
-    },
-    {
-      category: 'Now Showing',
-      title: 'Sonic the Hedgehog 2',
-      poster: '/images/SonictheHedgehog2.jpg',
-    },
-    {
-      category: 'Now Showing',
-      title: 'The Lost City',
-      poster: '/images/TheLostCity.jpg',
-    },
-    {
-      category: 'Now Showing',
-      title: 'Uncharted',
-      poster: '/images/Uncharted.jpg',
-    },
-    {
-      category: 'Now Showing',
-      title: 'Spider-Man',
-      poster: '/images/Spider-Man.jpg',
-    },
-    {
-      category: 'Now Showing',
-      title: 'Dog',
-      poster: '/images/Dog.jpg',
-    },
-    {
-      category: 'Coming Soon',
-      title: 'The Batman',
-      poster: '/images/TheBatman.jpg',
-    },
-  ];
+  // const movieList = [
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'Father Stu',
+  //     poster: '/images/FatherStu.jpg',
+  //   },
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'Fantastic Beasts',
+  //     poster: '/images/FantasticBeasts.jpg',
+  //   },
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'Ambulance',
+  //     poster: '/images/Ambulance.jpg',
+  //   },
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'Sonic the Hedgehog 2',
+  //     poster: '/images/SonictheHedgehog2.jpg',
+  //   },
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'The Lost City',
+  //     poster: '/images/TheLostCity.jpg',
+  //   },
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'Uncharted',
+  //     poster: '/images/Uncharted.jpg',
+  //   },
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'Spider-Man',
+  //     poster: '/images/Spider-Man.jpg',
+  //   },
+  //   {
+  //     category: 'Now Showing',
+  //     title: 'Dog',
+  //     poster: '/images/Dog.jpg',
+  //   },
+  //   {
+  //     category: 'Coming Soon',
+  //     title: 'The Batman',
+  //     poster: '/images/TheBatman.jpg',
+  //   },
+  // ];
 
   return (
     <Container>
